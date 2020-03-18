75bd87f1fbdcb965d564ec661d7d82ad1d065e40
qijianchao <jianchaoqi@hotmail.com>
2020-03-18 10:09:27 +0800

    Mybatis学习实验

--- src/main/java/com/didispace/chapter1/mapper/UserMapper.java
+++ src/main/java/com/didispace/chapter1/mapper/UserMapper.java
@@ -1,10 +1,10 @@
 package com.didispace.chapter1.mapper;
 
 import com.didispace.chapter1.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
@@ -12,5 +12,26 @@ public interface UserMapper {
     User findByName(@Param("name") String name);
 
     @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=int.class)
     int insert(@Param("name") String name, @Param("age") Integer age);
+
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name,jdbcType=VARCHAR},#{age,jdbcType=INTEGER})")
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=int.class)
+    int insertByMap(Map<String, Object> map);
+
+    @Insert("INSERT INTO USER(NAME,AGE) VALUES(#{name,jdbcType=VARCHAR},#{age,jdbcType=INTEGER})")
+    @Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
+    int insertByUser(User user);
+
+    @Update("UPDATE USER SET name=#{name},age=#{age} where id=#{id}")
+    void update(User user);
+
+    @Delete("DELETE FROM USER WHERE id=#{id}")
+    void delete(Long id);
+
+    @Delete("DELETE FROM USER")
+    void deleteAll();
+
+    @Select("SELECT * FROM USER")
+    List<User> findAll();
 }
--- src/main/java/com/didispace/chapter1/web/UserMybatisController.java
+++ src/main/java/com/didispace/chapter1/web/UserMybatisController.java
@@ -21,18 +21,18 @@ public class UserMybatisController {
     }
 
     /**
-     * 处理"/usersJPA/"的GET请求，用来获取用户列表
+     * 处理"/usersMybatis/"的GET请求，用来获取用户列表
      * @return
      */
     @ApiOperation(value = "获取用户列表")
     @GetMapping("/")
-    public User getUserList() {
-        User r = userMapper.findByName("AAA");
+    public List<User> getUserList() {
+        List<User> r = userMapper.findAll();
         return r;
     }
 
     /**
-     * 处理"/usersJPA/"的POST请求，用来创建User
+     * 处理"/usersMybatis/"的POST请求，用来创建User
      * @param user
      * @return
      */
@@ -40,8 +40,36 @@ public class UserMybatisController {
     @PostMapping("/")
     public String postUser(@Valid @RequestBody com.didispace.chapter1.domain.User user) {
         // @RequestBody注解用来绑定通过http请求中application/json类型上传的数据
-        userMapper.insert(user.getName(), user.getAge());
-        return "success";
+        // int ret = userMapper.insert(user.getName(), user.getAge());
+        User u = new User(user.getName(),user.getAge());
+        int ret = userMapper.insertByUser(u);
+        return Integer.toString(ret) + " userId:" + Long.toString(u.getId());
+    }
+
+    /**
+     * 处理"/usersMybatis/name/"的POST请求，用来获取用户
+     * @return
+     */
+    @ApiOperation(value = "根据用户名称获取一个用户")
+    @PostMapping("/name/")
+    public User getUserByName(@RequestParam String name) {
+        User r = userMapper.findByName(name);
+        return r;
+    }
+
+    /**
+     * 处理"/usersMybatis/dellAll/"的POST请求，用来获取用户
+     * @return
+     */
+    @ApiOperation(value = "删除所有用户")
+    @PostMapping("/delAll/")
+    public String deleteAllUser(@RequestParam String yes) {
+        if(yes.equals("1")) {
+            userMapper.deleteAll();
+            return "yes";
+        } else {
+            return "no";
+        }
     }
 
 }
