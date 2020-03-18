9918f7dd4c9d398b6e438573bc869beb8ff365c6
qijianchao <jianchaoqi@hotmail.com>
2020-03-18 23:00:08 +0800

    修改为Eureka-Client项目，将服务自动注册

--- src/main/java/com/didispace/chapter1/Chapter1Application.java
+++ src/main/java/com/didispace/chapter1/Chapter1Application.java
@@ -4,7 +4,9 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableSwagger2Doc
 @MapperScan("com.didispace.chapter1.mapper")
 @SpringBootApplication
--- src/main/java/com/didispace/chapter1/web/HelloController.java
+++ src/main/java/com/didispace/chapter1/web/HelloController.java
@@ -1,5 +1,8 @@
 package com.didispace.chapter1.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,15 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
     }
 }
